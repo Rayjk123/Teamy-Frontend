8535223aa99cd527248da7917648bb03efc1b8e2
Yang Liu <byliuyang11@gmail.com>
2018-01-12 01:07:17 -0500

    Add authorship

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import NavBar from './navbar/NavBar';
+import heart from './heart.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <NavBar></NavBar>
+          <NavBar/>
+          <div className={'authorship'}>
+            Made with <img src={heart}/> by <a className={'author-github'} href={'https://github.com/byliuyang'}>Yang Liu</a>
+          </div>
       </div>
     );
   }
