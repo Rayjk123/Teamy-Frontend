fb7d6428ff5d4225b6ff1b20309e374e828be811
Yang Liu <byliuyang11@gmail.com>
2018-01-12 01:16:07 -0500

    Add Projects

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import NavBar from './navbar/NavBar';
 import heart from './heart.svg';
 import './App.css';
+import Projects from "./projects/Projects";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <NavBar/>
-          <div className={'authorship'}>
-            Made with <img src={heart}/> by <a className={'author-github'} href={'https://github.com/byliuyang'}>Yang Liu</a>
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+              <NavBar/>
+              <Projects/>
+              <div className={'authorship'}>
+                Made with <img src={heart}/> by <a className={'author-github'} href={'https://github.com/byliuyang'}>Yang
+                Liu</a>
+              </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Projects extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Projects;
