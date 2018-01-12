5dce7887fd99984daac5e937105b3dec304b2ed7
Yang Liu <byliuyang11@gmail.com>
2018-01-12 00:58:08 -0500

    Add NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from './navbar/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <NavBar></NavBar>
       </div>
     );
   }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import './NavBar.css';
+import logo from './logo.svg';
+import notification from './notification.svg';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menu: {
+                selected: 0,
+                items: [
+                    {
+                        name: 'Projects',
+                        link: '/'
+                    },
+                    {
+                        name: 'Activities',
+                        link: '/'
+                    },
+                    {
+                        name: 'Team',
+                        link: '/'
+                    }
+                ]
+            }
+        };
+    }
+
+    _renderMenuItems() {
+        return this.state.menu.items.map((item, index) =>
+            <li key={index} className={index === this.state.menu.selected ? 'selected' : ''}>
+                {item.name}
+            </li>);
+    }
+
+    render() {
+        return (
+            <div className={'NavBar'}>
+                <div className={'center'}>
+                    <div className={'logo'}>
+                        <img src={logo}/>
+                    </div>
+                    <ul className={'menu'}>
+                        {this._renderMenuItems()}
+                    </ul>
+                    <div className={'user-info'}>
+                        <div className={'notification'}>
+                            <img src={notification}/>
+                        </div>
+                        <div className={'profile'}>
+                            <img />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
